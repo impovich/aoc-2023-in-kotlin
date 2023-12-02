13ab96ff20ca8b7df1dbb6210680a377d2910aad
impovich <impovich2@gmail.com>
2023-12-02 15:12:19 +0100

    minor improvement

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -4,21 +4,21 @@ import println
 import readInput
 
 fun main() {
-    fun part1(input: List<String>): Int = input.map {
-        val first = it.find(Char::isDigit)
-        val second = it.findLast(Char::isDigit)
-        "$first$second".toInt()
-    }.reduce{acc, i -> acc + i}
+    fun part1(input: List<String>): Int =
+        input.sumOf {
+            val first = it.find(Char::isDigit)
+            val second = it.findLast(Char::isDigit)
+            "${first}$second".toInt()
+        }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("/day01/Day01_test")
-    check(part1(testInput) == 142)
+    check(part1(readInput("/day01/Day01_test")) == 142)
+    check(part2(readInput("/day01/Day01_2_test")) == 281)
 
-    val input = readInput("/day01/Day01")
-    part1(input).println()
-    //part2(input).println()
+    part1(readInput("/day01/Day01")).println() // answer = 54990
+    part2(readInput("/day01/Day01_2")).println()
 }
