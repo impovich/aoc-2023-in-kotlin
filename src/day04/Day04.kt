8b1f4696767f33abdfcc28694fb9d26a99565035
impovich <impovich2@gmail.com>
2023-12-05 19:39:45 +0100

    solutions for day 4

--- src/day04/Day04.kt
+++ src/day04/Day04.kt
@@ -2,19 +2,42 @@ package day04
 
 import println
 import readInput
+import java.util.*
 
 
-class LotteryPointsService(val winCards: Map<Int, LotteryCard>) {
+class LotteryService(val winCards: Map<Int, LotteryCard>) {
 
-    fun getPoints(playerCards: List<LotteryCard>): Int =
-        playerCards.map(::getPoints).sum()
+    fun playForCards(playerCards: List<LotteryCard>): Int {
+        //TODO: optimize it(get rid of queue)
+        val queue = LinkedList<LotteryCard>()
+        queue.addAll(playerCards)
 
-    private fun getPoints(playerCard: LotteryCard): Int =
-        winCards[playerCard.id]?.let { winCard ->
-            playerCard.numbers.intersect(winCard.numbers).let {
-                if (it.isNotEmpty()) it.fold(0) { acc, _ -> if (acc == 0) 1 else acc * 2 } else 0
+        var cardCounter = 0
+        while (queue.isNotEmpty()) {
+            val lotteryCard = queue.poll()
+            val matchingNumbers = intersectNumbers(lotteryCard).count()
+
+            if (matchingNumbers != 0) {
+                for (i in lotteryCard.id..<lotteryCard.id + matchingNumbers) {
+                    playerCards[i].also {
+                        queue.add(it)
+                        cardCounter++
+                    }
+                }
             }
-        } ?: 0
+        }
+
+        return cardCounter + playerCards.size
+    }
+
+    fun playForPoints(playerCards: List<LotteryCard>): Int =
+        //sumOf bug - https://youtrack.jetbrains.com/issue/KT-46360
+        playerCards.map(::intersectNumbers).map {
+            if (it.isNotEmpty()) it.fold(0) { acc, _ -> if (acc == 0) 1 else acc * 2 } else 0
+        }.sum()
+
+    private fun intersectNumbers(playerCard: LotteryCard): Set<Int> =
+        winCards[playerCard.id]?.let { winCard -> playerCard.numbers.intersect(winCard.numbers.toSet()) } ?: emptySet()
 }
 
 data class LotteryCard(val id: Int, val numbers: List<Int>)
@@ -24,15 +47,23 @@ fun main() {
     fun part1(input: List<String>): Int {
         val (winCards, playerCard) = input.parse()
 
-        return LotteryPointsService(winCards.associateBy(LotteryCard::id)).getPoints(playerCard)
+        return LotteryService(winCards.associateBy(LotteryCard::id)).playForPoints(playerCard)
+    }
+
+    fun part2(input: List<String>): Int {
+        val (winCards, playerCard) = input.parse()
+
+        return LotteryService(winCards.associateBy(LotteryCard::id)).playForCards(playerCard)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("/day04/Day04_test")
     check(part1(testInput) == 13)
+    check(part2(testInput) == 30)
 
     val input = readInput("/day04/Day04")
     part1(input).println() //answer: 23673
+    part2(input).println() //answer: 12263631
 }
 
 private fun Iterable<String>.parse(): Pair<List<LotteryCard>, List<LotteryCard>> {
